3fc6036e58639e3a36f3e153be73f30883e7f3ad
IValenzuela30 <Ivalenzuela30@mail.pima.edu>
2025-02-26 23:14:03 -0700

    Update cis129_lab03_coffeeShop.py
    Added two new items to the menu, Cheese danishes and Croissants
    Changed the theme of the coffee shop
    Added a personalized message

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -1,25 +1,36 @@
 num_coffees = int(input("number of coffees purchased? "))
 num_muffins = int(input("number of muffins purchased? "))
+num_CheeseDanish = int(input("number of Cheese Danishes purchased? "))
+num_Croissants = int(input("number of croissants purchased? "))
 
 coffee_price = 5.00
 muffin_price = 4.00
+CheeseDanish_price = 3.00
+Croissant_price = 2.00
 tax_rate = 0.06
 
 coffee_total = num_coffees * coffee_price
 muffin_total = num_muffins * muffin_price
-subtotal = coffee_total + muffin_total
+CheeseDanish_total = num_CheeseDanish * CheeseDanish_price
+Croissant_total = num_Croissants * Croissant_price
+subtotal = coffee_total + muffin_total + CheeseDanish_total + Croissant_total
 tax = subtotal * tax_rate
 total = subtotal + tax
 
 print("*******************************************")
-print(" My coffee and muffin shop")
+print(" Dunkin Muffins ")
 print(f"Number of coffees purchased: {num_coffees}")
 print(f"Number of muffins purchased: {num_muffins}")
+print(f"Number of Cheese Danishes purchased: {num_CheeseDanish}")
+print(f"Number of Croissants purchased: {num_Croissants}")
 print("********************************************")
-print(" My Coffee and Muffin Shop Receipt")
+print(" Dunkin Muffins Reciept")
 print(f" {num_coffees} Coffee at ${coffee_price} each: ${coffee_total:.2f}")
 print(f" {num_muffins} Muffins at ${muffin_price} each: ${muffin_total:.2f}")
+print(f" {num_CheeseDanish} Cheese Danishes at ${CheeseDanish_price} each: ${CheeseDanish_total}")
+print(f" {num_Croissants} Croissants at ${Croissant_price} each: ${CheeseDanish_total}")
 print(f" 6% tax: ${tax:.2f}")
 print(" ---------")
 print(f" Total: ${total:.2f}")
 print("********************************************")
+print("Thank you for dunking by Dunkin Muffins!!!")
