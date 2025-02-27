1b98b366d001ddc321dc71047706c09d5dee4ca6
IValenzuela30 <Ivalenzuela30@mail.pima.edu>
2025-02-27 11:00:53 -0700

    Update cis129_lab03_coffeeShop.py
    Added tripple doc string and in line comments

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -1,14 +1,24 @@
+"""
+script: CIS129 Module 3 Lab
+author: Israel Valenzuela
+date: 10/11/2023
+description: This program calculates the total cost of a customer's order at Dunkin Muffins, including tax.
+"""
+
+# Input
 num_coffees = int(input("number of coffees purchased? "))
 num_muffins = int(input("number of muffins purchased? "))
 num_CheeseDanish = int(input("number of Cheese Danishes purchased? "))
 num_Croissants = int(input("number of croissants purchased? "))
 
+# Constants
 coffee_price = 5.00
 muffin_price = 4.00
 CheeseDanish_price = 3.00
 Croissant_price = 2.00
 tax_rate = 0.06
 
+# Calculate totals
 coffee_total = num_coffees * coffee_price
 muffin_total = num_muffins * muffin_price
 CheeseDanish_total = num_CheeseDanish * CheeseDanish_price
@@ -17,6 +27,7 @@ subtotal = coffee_total + muffin_total + CheeseDanish_total + Croissant_total
 tax = subtotal * tax_rate
 total = subtotal + tax
 
+# Output
 print("*******************************************")
 print(" Dunkin Muffins ")
 print(f"Number of coffees purchased: {num_coffees}")
